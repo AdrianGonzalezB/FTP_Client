82755448f7c4145ad405f6ae0b37e952a55bdb56
BryanJT01 <bryanmichaeljumbo@paucasesnovescifp.cat>
2022-02-04 18:32:32 +0100

    Prueba sin errores

---
+++ src/main/java/Prueba/FtpUtil.java
@@ -0,0 +1,145 @@
+package Prueba;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+ 
+import org.apache.commons.net.ftp.FTP;
+import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPFile;
+import org.apache.commons.net.ftp.FTPReply;
+ 
+/**
+   * Clase de herramientas de descarga de descarga de FTP
+ */
+public class FtpUtil {
+ 
+	/** 
+	  * Descripción: Cargar archivos al servidor FTP 
+	  * @Param Host FTP Server HostName 
+	  * @Param Port FTP Server Port 
+	  * @Param username FTP Cuenta de inicio de sesión 
+	  * @Param Password FTP Iniciar sesión Contraseña 
+	  * @Param BASEPATH FTP Server Base Directorio
+	  * @Param FilePath FTP Server File Ruta de almacenamiento. El camino del archivo es BASEPATH + FILEPATH
+	  * @Param filename Sube los nombres de archivos en el servidor FTP 
+	  * @Param entrada de entrada flujo 
+	  * @Return devuelve con éxito, de lo contrario devuelve FALSO 
+	 */  
+	public static boolean uploadFile(String host, int puerto, String username, String password, String basePath,
+			String filePath, String filename, InputStream input) throws IOException {
+		boolean result = false;
+		FTPClient ftp = new FTPClient();
+		try {
+			int reply;
+			 ftp.connect (host, puerto); // Conecte el servidor FTP
+			 // Si usa el puerto predeterminado, puede usar ftp.connect (host) para conectar directamente el servidor FTP.
+			 ftp.login(username, password); // Iniciar sesión
+			reply = ftp.getReplyCode();
+			if (!FTPReply.isPositiveCompletion(reply)) {
+				ftp.disconnect();
+				return result;
+			}
+			 // Cambia al directorio de carga
+			if (!ftp.changeWorkingDirectory(basePath+filePath)) {
+				 // Si el directorio no tiene un directorio de creación
+				String[] dirs = filePath.split("/");
+				String tempPath = basePath;
+				for (String dir : dirs) {
+					if (null == dir || "".equals(dir)) continue;
+					tempPath += "/" + dir;
+					 if (! ftp.changeWorkingDirectory (tempPath)) {// facturado al directorio, lo que indica que el directorio no existe
+						 if (! ftp.makeDirectory (tempPath)) {// crea un directorio
+							 // Si el directorio de archivo de creación falla, vuelve
+							 System.out.println ("Crear directorio de archivos" + tempPath + "Fallo");
+							return result;
+						} else {
+							 // existe el directorio, luego ingrese el directorio directamente
+							ftp.changeWorkingDirectory(tempPath);	
+						}
+					}
+				}
+			}
+			 // Establecer el tipo de archivo de carga al tipo binario
+			ftp.setFileType(FTP.BINARY_FILE_TYPE);
+			 			//subir archivos 
+			if (!ftp.storeFile(filename, input)) {
+				return result;
+			}
+			input.close();
+			ftp.logout();
+			result = true;
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (ftp.isConnected()) {
+                            ftp.disconnect();
+			}
+		}
+		return result;
+	}
+	
+	/** 
+	  * Descripción: Descargar archivo desde el servidor FTP 
+	  * @Param Host FTP Server HostName 
+	 * @Param Port FTP Server Port 
+	  * @Param username FTP Cuenta de inicio de sesión 
+	  * @Param Password FTP Iniciar sesión Contraseña 
+	  * @Param REMOTEPATH FTP Server Ruta relativa 
+	  * @Param filename para descargar el nombre del archivo 
+	  * @Param localpath descargas a caminos locales 
+	 * @return 
+	 */  
+	public static boolean downloadFile(String host, int port, String username, String password, String remotePath,
+			String fileName, String localPath) throws IOException {
+		boolean result = false;
+		FTPClient ftp = new FTPClient();
+		try {
+			int reply;
+			ftp.connect(host, port);
+			 // Si usa el puerto predeterminado, puede usar ftp.connect (host) para conectar directamente el servidor FTP.
+			 ftp.login (username, password); // Iniciar sesión
+			reply = ftp.getReplyCode();
+			if (!FTPReply.isPositiveCompletion(reply)) {
+				ftp.disconnect();
+				return result;
+			}
+			 ftp.changeWorkingDirectory (remotePath); // Transferencia al directorio del servidor FTP
+			FTPFile[] fs = ftp.listFiles();
+			for (FTPFile ff : fs) {
+				if (ff.getName().equals(fileName)) {
+					File localFile = new File(localPath + "/" + ff.getName());
+ 
+					OutputStream is = new FileOutputStream(localFile);
+					ftp.retrieveFile(ff.getName(), is);
+					is.close();
+				}
+			}
+ 
+			ftp.logout();
+			result = true;
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (ftp.isConnected()) {
+                            ftp.disconnect();
+			}
+		}
+		return result;
+	}
+ 
+	 // Función principal de prueba de archivo de archivo FTP
+	public static void main(String[] args) throws IOException {
+		try {  
+	        FileInputStream in=new FileInputStream(new File("D:\\Tomcat 5.5\\pictures\\t0176ee418172932841.jpg"));  
+	         boolean flag = uploadFile ("192.168.11.128", 21, "username", "password", "/www/images", "/2017/11/19","hello.jpg", in);  
+	        System.out.println(flag);  
+	    } catch (FileNotFoundException e) {  
+	        e.printStackTrace();  
+	    }  
+	}
+}
---
+++ src/main/java/Prueba/SFTPUtil.java
@@ -0,0 +1,205 @@
+package Prueba;
+
+import java.io.File;  
+import java.io.FileInputStream;  
+import java.io.FileNotFoundException;  
+import java.io.FileOutputStream;  
+import java.io.IOException;  
+import java.io.InputStream;  
+import java.util.Properties;  
+import java.util.Vector;  
+  
+import org.apache.commons.io.IOUtils;  
+import org.slf4j.Logger;  
+import org.slf4j.LoggerFactory;  
+  
+import com.jcraft.jsch.Channel;  
+import com.jcraft.jsch.ChannelSftp;  
+import com.jcraft.jsch.JSch;  
+import com.jcraft.jsch.JSchException;  
+import com.jcraft.jsch.Session;  
+import com.jcraft.jsch.SftpException; 
+/** 
+* Clase Descripción Herramientas SFTP
+*/
+public class SFTPUtil {
+    private transient Logger log = LoggerFactory.getLogger(this.getClass());  
+    
+    private ChannelSftp sftp;  
+        
+    private Session session;  
+         // ** Nombre de usuario de inicio de sesión SFTP * /    
+    private String username; 
+         // ** Contraseña de inicio de sesión SFTP * /    
+    private String password;  
+         // ** llave privada * /    
+    private String privateKey;  
+         // ** Dirección IP de la dirección del servidor SFTP * /    
+    private String host;  
+         // ** Puerto SFTP * /  
+    private int port;  
+        
+    
+    /**  
+           * Construir un objeto SFTP basado en la autenticación de contraseña  
+     */    
+    public SFTPUtil(String username, String password, String host, int port) {  
+        this.username = username;  
+        this.password = password;  
+        this.host = host;  
+        this.port = port;  
+    } 
+    
+    /**  
+           * Construye objetos SFTP basados ​​en la autenticación de clave 
+     */  
+    public SFTPUtil(String username, String host, int port, String privateKey) {  
+        this.username = username;  
+        this.host = host;  
+        this.port = port;  
+        this.privateKey = privateKey;  
+    }  
+    
+    public SFTPUtil(){}  
+    
+    
+    /** 
+           * Conecte el servidor SFTP 
+     */  
+    public void login(){  
+        try {  
+            JSch jsch = new JSch();  
+            if (privateKey != null) {  
+                                 jsch.addIdentity (privateKey); // Establecer la clave privada  
+            }  
+    
+            session = jsch.getSession(username, host, port);  
+           
+            if (password != null) {  
+                session.setPassword(password);    
+            }  
+            Properties config = new Properties();  
+            config.put("StrictHostKeyChecking", "no");  
+                
+            session.setConfig(config);  
+            session.connect();  
+              
+            Channel channel = session.openChannel("sftp");  
+            channel.connect();  
+    
+            sftp = (ChannelSftp) channel;  
+        } catch (JSchException e) {  
+            e.printStackTrace();
+        }  
+    }    
+    
+    /** 
+           * Cerrar conexión de conexión  
+     */  
+    public void logout(){  
+        if (sftp != null) {  
+            if (sftp.isConnected()) {  
+                sftp.disconnect();  
+            }  
+        }  
+        if (session != null) {  
+            if (session.isConnected()) {  
+                session.disconnect();  
+            }  
+        }  
+    }  
+ 
+    
+    /**  
+           * Cargue los datos del flujo de entrada a SFTP como un archivo. Patrimonio FOCAL = directorio BASEPATH +
+           * El camino básico de @param Basepath Server 
+           * @Param directorio Sube a este directorio  
+           * @Param SFTPFILENAME SMFP FIN DE FINILA NOMBRE  
+           * @Param en flujo de entrada  
+     */  
+    public void upload(String basePath,String directory, String sftpFileName, InputStream input) throws SftpException{  
+        try {   
+            sftp.cd(basePath);
+            sftp.cd(directory);  
+        } catch (SftpException e) { 
+                          // el directorio no existe, luego crea una carpeta
+            String [] dirs=directory.split("/");
+            String tempPath=basePath;
+            for(String dir:dirs){
+            	if(null== dir || "".equals(dir)) continue;
+            	tempPath+="/"+dir;
+            	try{ 
+            		sftp.cd(tempPath);
+            	}catch(SftpException ex){
+            		sftp.mkdir(tempPath);
+            		sftp.cd(tempPath);
+            	}
+            }
+        }  
+                 sftp.put(input, sftpFileName); // Subir archivo
+    } 
+    
+ 
+    /** 
+           * descargar archivo.
+           * @Param directorio de descarga de directorio  
+           * @Param DownloadFile Descargar archivo 
+           * @Param SaveFile existe camino local 
+     */    
+    public void download(String directory, String downloadFile, String saveFile) throws SftpException, FileNotFoundException{  
+        if (directory != null && !"".equals(directory)) {  
+            sftp.cd(directory);  
+        }  
+        File file = new File(saveFile);  
+        sftp.get(downloadFile, new FileOutputStream(file));  
+    }  
+    
+    /**  
+           * descargar archivo   
+           * @Param directorio de descarga de directorio 
+           * @Param DownloadFile Descargar Nombre del archivo 
+           * @Return Byte Array 
+     */  
+    public byte[] download(String directory, String downloadFile) throws SftpException, IOException{  
+        if (directory != null && !"".equals(directory)) {  
+            sftp.cd(directory);  
+        }  
+        InputStream is = sftp.get(downloadFile);  
+          
+        byte[] fileData = IOUtils.toByteArray(is);  
+          
+        return fileData;  
+    }  
+    
+    
+    /** 
+           * Borrar archivos   
+           * @Param directorio para eliminar el archivo donde se encuentra el archivo 
+           * @Param DeleteFile para eliminar archivos 
+     */  
+    public void delete(String directory, String deleteFile) throws SftpException{  
+        sftp.cd(directory);  
+        sftp.rm(deleteFile);  
+    }  
+    
+    
+    /** 
+           * Enumere los archivos en el directorio. 
+           * @Param directorio para enumerar el directorio 
+     * @param sftp 
+     */  
+    public Vector<?> listFiles(String directory) throws SftpException {  
+        return sftp.ls(directory);  
+    }  
+      
+         // Subir prueba de archivo
+    public static void main(String[] args) throws SftpException, IOException {  
+                 SFTPUtil sftp = new SFTPUtil ("Nombre de usuario", "Contraseña", "Dirección IP", 22);  
+        sftp.login();  
+                 File file = new File ("D: \\imagen \\ t0124dd095ceb042322.jpg");  
+        InputStream is = new FileInputStream(file);  
+          
+                 sftp.upload("Ruta básica", "Ruta de archivo", "test_sftp.jpg", is);  
+        sftp.logout();  
+    }  
+}
